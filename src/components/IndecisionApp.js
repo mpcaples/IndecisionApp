777ca392afd2bfd7c235489ebdc45e66a1c4ff28
Madeline Caples <madelinecaples@gmail.com>
2022-05-16 15:51:12 -0400

    Added react-modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,17 +4,11 @@ import AddOption from './AddOption';
 import Options from './Options'; 
 import Action from './Action'; 
 import Header from './Header'; 
-
+import OptionModal from './OptionModal'
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props); 
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this); 
-        this.handlePick = this.handlePick.bind(this)
-        this.handleAddOption = this.handleAddOption.bind(this)
-        this.handleDeleteOption = this.handleDeleteOption.bind(this)
-        this.state = {
-            options: props.options
-        }; 
+    state = {
+        options: this.props.options, 
+        selectedOption: undefined
     }
     componentDidMount() {
         try {
@@ -36,18 +30,21 @@ export default class IndecisionApp extends React.Component {
             localStorage.setItem('options', json); 
         }
     }
-    handleDeleteOptions() {
+    handleClearOption = () => {
+        this.setState(() => ({selectedOption: undefined}))
+    }
+    handleDeleteOptions = () => {
         this.setState(() => ({options: []})); 
     }
-    handleDeleteOption(optionToRemove) {
+    handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({options: prevState.options.filter((option) => optionToRemove !== option)})); 
     }
-    handlePick() {
+    handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length); 
         const option = this.state.options[randomNum];
-        alert(option); 
+        this.setState(() => ({selectedOption: option})); 
     }
-    handleAddOption(option) {
+    handleAddOption = (option) => {
         if (!option) {
             return 'Enter valid value to add item';
         } else if (this.state.options.indexOf(option) >-1) {
@@ -76,6 +73,10 @@ export default class IndecisionApp extends React.Component {
                 <AddOption
                    handleAddOption={this.handleAddOption} 
                 />
+                <OptionModal 
+                    selectedOption={this.state.selectedOption}
+                    handleClearOption={this.handleClearOption}
+                    />
             </div>
         );
     }
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react'; 
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionText}
-            <button 
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                Remove Option
-            </button>
-        </div>
-    )
-}
+const Option = (props) => (
+    <div>
+        {props.optionText}
+        <button 
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        >
+            Remove Option
+        </button>
+    </div>
+); 
 export default Option; 
